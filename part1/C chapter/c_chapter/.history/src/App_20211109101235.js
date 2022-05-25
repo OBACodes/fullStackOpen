07c4287330e045bf7e985235749bfc0f91c0334e
OBACodes <oba2311+1@gmail.com>
2022-05-25 17:17:44 +0300

    part1 added

---
+++ part1/C chapter/c_chapter/.history/src/App_20211109101235.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useState } from "react";
+
+const App = () => {
+	const [left, setLeft] = useState(0);
+	const [right, setRight] = useState(0);
+	const [allClicks, setAll] = useState([]);
+
+	const handleLeftClick = () => setClicks({ ...clicks, left: clicks.left + 1 });
+
+	const handleRightClick = () =>
+		setClicks({ ...clicks, right: clicks.right + 1 });
+
+	return (
+		<div>
+			{clicks.left}
+			<button onClick={handleLeftClick}>left</button>
+			<button onClick={handleRightClick}>right</button>
+			{clicks.right}
+		</div>
+	);
+};
+
+export default App;
---
+++ part1/C chapter/c_chapter/.history/src/App_20211109101254.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useState } from "react";
+
+const App = () => {
+	const [left, setLeft] = useState(0);
+	const [right, setRight] = useState(0);
+	const [allClicks, setAll] = useState([]);
+
+	const handleLeftClick = () => {
+		setAll(allClicks.concat("L"));
+		setLeft(left + 1);
+	};
+
+	const handleRightClick = () => {
+		setAll(allClicks.concat("R"));
+		setRight(right + 1);
+	};
+
+	return (
+		<div>
+			{clicks.left}
+			<button onClick={handleLeftClick}>left</button>
+			<button onClick={handleRightClick}>right</button>
+			{clicks.right}
+		</div>
+	);
+};
+
+export default App;
